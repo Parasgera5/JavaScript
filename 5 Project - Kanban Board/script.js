ceee07806ce1ae0022e064b0ec736d0b079a3345
Paras <parasgera2005@gmail.com>
2026-02-26 19:54:38 +0530

    Added Dragging functionality

---
+++ 5 Project - Kanban Board/script.js
@@ -0,0 +1,46 @@
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+
+const tasks = document.querySelectorAll(".task");
+tasks.forEach((task) => {
+    task.addEventListener("drag", (e) => {
+        // console.log("dragging", e);
+    })
+})
+// progress.addEventListener("dragenter", (e) => {
+//     progress.classList.add("hover-over");
+// })
+// progress.addEventListener("dragleave", (e) => {
+//     progress.classList.remove("hover-over");
+// })
+// todo.addEventListener("dragenter", (e) => {
+//     todo.classList.add("hover-over");
+// })
+// todo.addEventListener("dragleave", (e) => {
+//     todo.classList.remove("hover-over");
+// })
+// done.addEventListener("dragenter", (e) => {
+//     done.classList.add("hover-over");
+// })
+// done.addEventListener("dragleave", (e) => {
+//     done.classList.remove("hover-over");
+// })
+
+const addDragEventsOnColumn = (column) => {
+    column.addEventListener("dragenter", (e) => {
+        e.preventDefault();
+        column.classList.add("hover-over");
+    })
+    column.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    })
+
+    column.addEventListener("drop", (e) => {
+        e.preventDefault();
+    })
+}
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
